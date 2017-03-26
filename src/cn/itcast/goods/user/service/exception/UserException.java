dd6f10e9f5394bed8c59583c00684f24ce6765ee
Extreme <1690027871@qq.com>
2017-03-26 11:34:17 +0800

    all

---
+++ src/cn/itcast/goods/user/service/exception/UserException.java
@@ -0,0 +1,29 @@
+package cn.itcast.goods.user.service.exception;
+
+/**
+ * 用户业务异常
+ *
+ *
+ */
+public class UserException extends Exception {
+
+	public UserException() {
+		super();
+	
+	}
+
+	public UserException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public UserException(String message) {
+		super(message);
+
+	}
+
+	public UserException(Throwable cause) {
+		super(cause);
+
+	}
+}
